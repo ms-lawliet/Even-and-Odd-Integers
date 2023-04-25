f902226055f71a71c94bdbd9129c8eaf204bdd81
Ms Lawliet <alexieanoya@gmail.com>
2023-04-25 11:08:33 +0800

    wrote instructions for program

---
+++ program_code.py
@@ -0,0 +1,4 @@
+# Write a Python program that reads a text file named numbers.txt that contains 20 numbers. The program will create
+# two other text file; the first text file will be named even.txt that will contains all even numbers extracted from
+# the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the
+# numbers.txt.
